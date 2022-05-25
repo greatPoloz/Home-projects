d5073a2d621f066d7f39d3a46610c3347eefb3eb
Nata Poloz <103985050+greatPoloz@users.noreply.github.com>
2022-05-25 23:03:41 +0500

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-// 2. Написать функцию поиска наибольшего общего делителя.
-
-function nod(x, y) {
-    if (y > x) return nod (y, x)
-    if (!y) return x;
-	return nod(y, x % y);
-}
-console.log(nod(10, 25)); //5
