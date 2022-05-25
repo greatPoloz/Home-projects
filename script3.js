61036d66913795559e0b9bdeca493bac6d79c4d1
Nata Poloz <103985050+greatPoloz@users.noreply.github.com>
2022-05-25 23:03:53 +0500

    Delete script3.js

--- script3.js
+++
@@ -1,9 +0,0 @@
-// 3. Написать функцию для поиска максимальной цифры в числе.
-
-function maxDitig(n) {
-    if (n < 10)
-    return n;
-    return Math.max(n % 10, maxDitig(parseInt(n / 10)));
-}
-
-console.log(maxDitig(156)); //6
